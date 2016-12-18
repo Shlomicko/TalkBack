087f1006e5ddc8e0bc4efb52f8b1a4acf8cc50ba
lazycreep <lazycreep@users.noreply.github.com>
2016-12-18 12:52:37 +0200

    WPF Client App

---
+++ ClientSide/WPFClientDemo/Converters/ImageSourceCubeConverter.cs
@@ -0,0 +1,42 @@
+﻿using Backgammon;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFClientDemo.Converters
+{
+    public class ImageSourceCubeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            switch ((int)value)
+            {
+                case 1:
+            return @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\1.png";
+                case 2:
+                    return @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\2.png";
+                case 3:
+                 return   @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\3.png";
+                case 4:
+                    return @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\4.png";
+                case 5:
+                    return @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\5.png";
+                case 6:
+                    return @"C:\Users\hen\Desktop\ClientSideTester\WPFClientDemo\Images\6.png";
+                default:
+                    return new Exception("something is wrong with the steps values");
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClientSide/WPFClientDemo/Converters/VisibilityConverterForOneBlack.cs
@@ -0,0 +1,31 @@
+﻿using Backgammon.PieceFolder;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPFClientDemo.Converters
+{
+    public class VisibilityConverterForOneBlack : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            List<IPiece> blackPieces = (List<IPiece>)value;
+            if(blackPieces.Count > 0)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
